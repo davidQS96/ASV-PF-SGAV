225d6eca8c001a03d1e7aa4c1eadfbae417686b0
ema108 <72424177+ema108@users.noreply.github.com>
2021-01-23 21:37:55 -0600

    Funcción de reconocimiento final
    Misma entrada
    Comando: 1 a 4
    Comando 5 y 6

--- funRecono.py
+++ funRecono.py
@@ -4,6 +4,30 @@ from skimage.io import imread
 import matplotlib.pyplot as plt
 
 
+def signo(img,maskc,cont):
+    count = 0
+    maskc = np.zeros(img.shape, dtype=np.uint8)
+    cv2.drawContours(maskc, cont, 0, (255, 255, 255), cv2.FILLED)
+    maskc = cv2.cvtColor(maskc, cv2.COLOR_RGB2GRAY)
+    corte = cv2.bitwise_or(img, img, mask=maskc)
+    gray = cv2.cvtColor(corte, cv2.COLOR_RGB2GRAY)
+    contorno, _ = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contorno:
+        approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
+        area = cv2.contourArea(cnt)
+        if len(approx) == 4:
+            if area < 20000 and area > 100:
+                valido = 1
+                comando2 = 6
+
+
+        if len(approx) == 12:
+            if area < 20000 and area > 100:
+                valido = 1
+                comando2 = 5
+
+
+    return  valido,comando2
 
 
 def recono(img):
@@ -24,16 +48,30 @@ def recono(img):
     for cnt in contora:
         approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
         if len(approx) == 3:
-            comando = 2
+            valido,com2 = signo(img,maskora,[approx])
+            if valido == 1:
+                comando = 2
+                comando2 = com2
+                
         if len(approx) == 4:
-            comando = 1
-
+            valido,com2 = signo(img,maskora,[approx])
+            if valido == 1:
+                comando = 1
+                comando2 = com2
+                
     for cnt in contblu:
         approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
         if len(approx) == 3:
-            comando = 4
+            valido,com2 = signo(img,maskora,[approx])
+            if valido == 1:
+                comando = 4
+                comando2 = com2
+                
         if len(approx) == 4:
-            comando = 3
+            valido,com2 = signo(img,maskora,[approx])
+            if valido == 1:
+                comando = 3
+                comando2 = com2
     
     
-    return comando
+    return comando,comando2
