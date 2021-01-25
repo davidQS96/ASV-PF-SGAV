2959c97e5310cd383090a8a0cb84ea9e2bb48bb0
ema108 <72424177+ema108@users.noreply.github.com>
2021-01-25 00:13:35 -0600

    Sal y pimienta v2

--- Archivos temporales/pruebaSalPimienta.py
+++ Archivos temporales/pruebaSalPimienta.py
@@ -2,28 +2,28 @@ import cv2
 import random
 import numpy
 
-perc = .03
+perc = .05
 
-size = (750, 500)
+size = (567, 378)
 l = []
 
-totPixels = 750 * 500
+totPixels = 567 * 378
 totNoise = totPixels * 100
 
 print(totPixels, totNoise)
 
 img = numpy.zeros([size[1], size[0], 3])
 
-img[:, :, 0] = numpy.ones([size[1], size[0]])
-img[:, :, 1] = numpy.ones([size[1], size[0]])
-img[:, :, 2] = numpy.ones([size[1], size[0]])
+img[:, :, 0] = numpy.zeros([size[1], size[0]])
+img[:, :, 1] = numpy.zeros([size[1], size[0]])
+img[:, :, 2] = numpy.zeros([size[1], size[0]])
 
 for i in range(img.shape[1]):
     for j in range(img.shape[0]):
         if random.random() <= perc:
-            img[j, i] = numpy.array((0, 0, 0))
+            img[j, i] = numpy.array((255, 255, 255))
 
-
+cv2.imwrite("sal3.jpg", img)
 cv2.imshow("image", img)
 cv2.waitKey()
 
